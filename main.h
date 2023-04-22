51e8858ce710830addda59c475268f47027b01bd
princewilliyk <Uka Princewill>
2023-04-22 09:13:37 -0700

    _printf without %

--- main.h
+++ main.h
@@ -6,5 +6,5 @@
 #include <unistd.h>
 int _putchar(char c);
 int print_string(char *s);
-
+int _printf(const char *format, ...);
 #endif
--- printf.c
+++ printf.c
@@ -7,4 +7,13 @@
  */
 
 int _printf(const char *format, ...)
-{}
+{
+	unsigned int i = 0, count = 0;
+
+	for (; format[i] != 0; i++)
+	{
+		if (format[i] != '%')
+			count += _putchar(format[i]);
+	}
+	return (count);
+}
