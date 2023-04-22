72d5d813dd602c4b43b49d441a90031eaff8d348
Olawole Olawale Martins <olawalemart@gmail.com>
2023-04-22 15:01:37 +0000

    printf shell file

---
+++ printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _printf - prints a formated text with arguements passed
+ * @format: formated string - formatting is optional
+ * Return: Number of characters printed to screen
+ */
+
+int _printf(const char *format, ...)
+{}
