370bdc8280092bc8b7de4dd576d16a32adaa7e3b
Olawole Olawale Martins <olawalemart@gmail.com>
2023-04-22 18:25:09 +0000

    print f with % %c and %s v2

--- printf.c
+++ printf.c
@@ -11,6 +11,8 @@ int _printf(const char *format, ...)
 	unsigned int i = 0, count = 0;
 	va_list args;
 
+	if (format == NULL)
+		return (-1);
 	va_start(args, format);
 	for (; format[i] != 0; i++)
 	{
