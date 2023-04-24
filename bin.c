cb9501b720ab953905ecdfa9233973bc76a471f9
Olawole Olawale Martins <olawalemart@gmail.com>
2023-04-24 11:48:42 +0000

    binary updated

--- bin.c
+++ bin.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * print_bin - function that prints a binary value
- * @args: an argument list
+ * printb - function that prints a binary value
+ * @n: unsigned int to convert
  * Return: returns the amount of elements printed
  */
-int print_bin(va_list args)
+int printb(unsigned int n)
 {
-	unsigned int n = va_arg(args, unsigned int);
 	unsigned int count = 0;
 
 	if (n < 2)
@@ -15,15 +14,28 @@ int print_bin(va_list args)
 		count += _putchar(n + 48);
 	}
 	else
-	if ((n / 2) < 2)
+	if (n / 2 < 2)
 	{
-		count += _putchar(n + 48);
-		count += _putchar((n % 2) + 48);
+		count += _putchar(n / 2 + 48);
+		count += _putchar(n % 2 + 48);
 	}
 	else
 	{
-		count += print_bin(n / 2);
-		count += _putchar((n % 2) + 48);
+		count += printb(n / 2);
+		count += _putchar(n % 2 + 48);
 	}
 	return (count);
+}
+
+/**
+ * print_bin - prints undigned int from va_list in binary
+ *@args: va_list arguement
+ * Return: number of items printed
+ */
+
+int print_bin(va_list args)
+{
+	unsigned n = va_arg(args, unsigned int);
+
+	return (printb(n));
 }
