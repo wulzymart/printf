7d8e6b0aacb6f551289e15c06b55146da5149249
Olawole Olawale Martins <olawalemart@gmail.com>
2023-04-24 16:41:59 +0000

    using local buffer with write

--- _putchar.c
+++ _putchar.c
@@ -8,5 +8,19 @@
 
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static int i;
+	static char buff[1024];
+
+	if (i >= 1024 || c == -1)
+	{
+		write(1, buff, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buff[i] = c;
+		write(1, &buff[i], 1);
+		i++;
+	}
+	return (1);
 }
